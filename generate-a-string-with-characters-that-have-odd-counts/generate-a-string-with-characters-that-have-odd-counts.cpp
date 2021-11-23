7bb0b4b87e247ca71ae43280d0cdcdcb2acf9fee
Prasoon Soni <prasoonsoni03@gmail.com>
2021-11-23 23:38:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (28.68%) - LeetHub

---
+++ generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string result = "";
+        if(n%2!=0){
+            for(int i=0;i<n;i++){
+                result += "a";
+            }
+        } else {
+            result += "b";
+            for(int i=0;i<n-1;i++){
+                result += "a";
+            }
+        }
+        return result;
+    }
+};
