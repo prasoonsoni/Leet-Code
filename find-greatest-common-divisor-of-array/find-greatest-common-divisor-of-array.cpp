9cdd6b95974e1bf73e028dca33a2c1d489a76127
Prasoon Soni <prasoonsoni03@gmail.com>
2021-11-25 00:54:41 +0530

    Time: 4 ms (90.73%), Space: 12.5 MB (62.11%) - LeetHub

--- find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -4,12 +4,15 @@ public:
         sort(nums.begin(), nums.end());
         int max_num = nums[nums.size()-1];
         int min_num = nums[0];
-        int gcd = INT_MIN;
-        for(int i=1;i<=min_num*max_num;i++){
+        /*int gcd = INT_MIN;
+        for(int i=1;i<=max(max_num,min_num);i++){
             if(min_num%i==0 && max_num%i==0){
                 gcd = max(gcd,i);
             }
         }
-        return gcd;
+        return gcd;*/
+        // Another methor with built in library
+        
+        return __gcd(max_num, min_num);
     }
 };
