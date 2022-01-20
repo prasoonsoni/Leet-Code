898cadf3fdad4a79b711fdd7b9448844fa325406
Prasoon Soni <prasoonsoni03@gmail.com>
2022-01-20 21:49:23 +0530

    Time: 15 ms (24.55%), Space: 10.1 MB (12.05%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1){return nums.at(0);}
+        unordered_map<int, int> um;
+        for(int i=0;i<nums.size();i++){
+            um[nums[i]]++;
+        }
+        for(auto it:um){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
